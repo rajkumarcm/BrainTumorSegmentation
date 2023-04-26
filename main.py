1ee43bf2c3a6cb6211f9521421d3d61ff9d75202
Rajkumar Conjeevaram Mohan <rajkumarcm@yahoo.com>
2023-04-26 12:56:25 -0400

    Created Object style code

--- main.py
+++ main.py
@@ -4,136 +4,19 @@ from matplotlib import pyplot as plt
 import nibabel
 from skimage.transform import resize
 import os
+import time
 os.environ['CUDA_VISIBLE_DEVICES']='1'
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH']='true'
 # os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 from os.path import abspath, join
 from os import listdir
 import tensorflow as tf
+from tensorflow.keras.layers import InputLayer
 from tensorflow.keras.layers import *
 from tensorflow.keras import Sequential
 from sklearn.model_selection import train_test_split
 
 
-#%%
-DATA_DIR = "/home/raj/from_host/MICCAI_BraTS2020_TrainingData/"
-DATA_DIR = r"C:\Users\Rajkumar\tf_container\MICCAI_BraTS2020_TrainingData"
-image_fnames = os.listdir(DATA_DIR)
-IMG_SIZE = 120
-
-#%%
-tmp_fname = image_fnames[0]
-img_fname = tmp_fname + os.path.sep + tmp_fname + "_t1.nii"
-file1 = nibabel.load(join(DATA_DIR, img_fname))
-tmp_img = file1.get_fdata()
-
-#%%
-slice = tmp_img[..., 60]  # 10 -> random channel
-fig = plt.figure(figsize=(4, 4))
-plt.imshow(slice, cmap="bone")
-plt.axis("off")
-fig.tight_layout()
-plt.show()
-
-#%%
-annt_fname = tmp_fname + os.path.sep + tmp_fname + "_seg.nii"
-seg1 = nibabel.load(join(DATA_DIR, annt_fname))
-tmp_seg = seg1.get_fdata()
-
-#%%
-fig = plt.figure()
-plt.imshow(tmp_seg[..., 60])
-plt.axis("off")
-fig.tight_layout()
-plt.show()
-
-#%%
-seg_indices = np.where(tmp_seg > 0)
-min_x, max_x = np.min(seg_indices[0]), np.max(seg_indices[0])
-min_y, max_y = np.min(seg_indices[1]), np.max(seg_indices[1])
-min_z, max_z = np.min(seg_indices[2]), np.max(seg_indices[2])
-
-#%%
-recons_img = np.zeros_like(tmp_img)
-recons_img[tmp_seg > 0] = tmp_img[tmp_seg > 0]
-
-#%%
-fig, axes = plt.subplots(1, 2)
-axes[0].imshow(tmp_img[..., 60])
-axes[0].set_title("Original Image")
-axes[1].imshow(recons_img[..., 60])
-axes[1].set_title("Segmented Image")
-axes[0].axis("off")
-axes[1].axis("off")
-fig.tight_layout()
-plt.show()
-
-#%%
-mask = np.ma.masked_where(tmp_seg > 0, tmp_seg)
-
-#%%
-fig = plt.figure()
-plt.imshow(tmp_img[..., 60], interpolation='none')
-plt.imshow(mask[..., 60], cmap="gray", alpha=0.7, interpolation='none')
-plt.axis("off")
-fig.tight_layout()
-plt.show()
-
-#%%
-# from nilearn import plotting
-#
-# display = plotting.plot_anat(join(DATA_DIR, img_fname))
-# display.add_overlay(join(DATA_DIR, annt_fname), cmap="hsv", colorbar=True)
-# plt.show()
-
-#%%
-BATCH_SIZE = 5
-PREFETCH = 2
-EPOCHS = 100
-DATA_PCALLS = 5
-
-#%%
-image_fnames = os.listdir(DATA_DIR)
-tr_fnames, ts_fnames = train_test_split(image_fnames, test_size=0.3)
-tr_fnames, vl_fnames = train_test_split(tr_fnames, test_size=0.3)
-
-def datagen(fnames):
-    for i in range(len(fnames)):
-        img_fname = fnames[i].decode() + os.path.sep + fnames[i].decode() + "_t1.nii"
-        seg_fname = fnames[i].decode() + os.path.sep + fnames[i].decode() + "_seg.nii"
-        img = nibabel.load(join(DATA_DIR, img_fname))
-        img = img.get_fdata().astype(np.float32)
-        img = resize(img, [IMG_SIZE, IMG_SIZE, 115])
-
-        seg = nibabel.load(join(DATA_DIR, seg_fname))
-        seg = seg.get_fdata().astype(np.int64)
-        seg = resize(seg, [IMG_SIZE, IMG_SIZE, 115])
-
-        yield img, seg
-
-# for tmp_x, tmp_y in datagen(tr_fnames):
-#     print(tmp_x.shape)
-#     print(tmp_y.shape)
-
-tr_dset = tf.data.Dataset.from_generator(datagen, output_types=((tf.float32, tf.int64)),
-                                         output_shapes=((IMG_SIZE, IMG_SIZE, 115),
-                                                        (IMG_SIZE, IMG_SIZE, 115)),
-                                         args=(tr_fnames,))
-tr_dset = tr_dset.batch(BATCH_SIZE, num_parallel_calls=DATA_PCALLS).prefetch(PREFETCH)
-
-vl_dset = tf.data.Dataset.from_generator(datagen, output_types=((tf.float32, tf.int64)),
-                                         output_shapes=((IMG_SIZE, IMG_SIZE, 115),
-                                                        (IMG_SIZE, IMG_SIZE, 115)),
-                                         args=(vl_fnames,))
-vl_dset = vl_dset.batch(BATCH_SIZE, num_parallel_calls=DATA_PCALLS).prefetch(PREFETCH)
-
-ts_dset = tf.data.Dataset.from_generator(datagen, output_types=((tf.float32, tf.int64)),
-                                         output_shapes=((IMG_SIZE, IMG_SIZE, 115),
-                                                        (IMG_SIZE, IMG_SIZE, 115)),
-                                         args=(ts_fnames,))
-ts_dset = ts_dset.batch(BATCH_SIZE, num_parallel_calls=DATA_PCALLS).prefetch(PREFETCH)
-
-#%%
 class ConvLayer(tf.keras.layers.Layer):
     def __init__(self, filters, kernel_size=(3,3,1), init=None, padding='same'):
         super(ConvLayer, self).__init__()
@@ -151,7 +34,6 @@ class ConvLayer(tf.keras.layers.Layer):
         x = self.conv(x)
         x = self.mp(x)
         return x
-
 
 class Downsampling_block(tf.keras.layers.Layer):
     def __init__(self):
@@ -189,7 +71,7 @@ class TConvLayer(tf.keras.layers.Layer):
         self.tconv = tf.keras.layers.Conv3DTranspose(filters, kernel_size, padding='valid',
                                                       activation='selu', kernel_initializer=init,
                                                       bias_initializer=init)
-        self.us = UpSampling3D(size=(2,2,1))
+        self.us = UpSampling3D(size=(2,2,1), interpolation='bilinear')
 
     def __call__(self, inputs):
         x = self.us(self.tconv(inputs))
@@ -227,46 +109,117 @@ class Upsampling_block(tf.keras.layers.Layer):
         # x = self.tconv5(x)
         return x
 
-model = Sequential([InputLayer(input_shape=[IMG_SIZE, IMG_SIZE, 115, 1],
-                               batch_size=None),
-                    Downsampling_block(),
-                    Upsampling_block(),
-                    Conv3D(1, (7,7,1), padding='valid'),
-                    Conv3D(1, (7,7,1), padding='valid'),
-                    Conv3D(1, (5,5,1), padding='valid'),
-                    Reshape([IMG_SIZE, IMG_SIZE, 115])
-                    ])
 
-model.summary()
+class ImageSegmentation:
+    def __init__(self):
+        self.DATA_DIR = "/home/raj/from_host/MICCAI_BraTS2020_TrainingData/"
+        self.DATA_DIR = r"C:\Users\Rajkumar\tf_container\MICCAI_BraTS2020_TrainingData"
+        self.IMG_SIZE = 120
+
+        self.BATCH_SIZE = 5
+        self.PREFETCH = 2
+        self.EPOCHS = 100
+        self.DATA_PCALLS = 5
+
+        image_fnames = os.listdir(self.DATA_DIR)
+
+        tr_fnames, ts_fnames = train_test_split(image_fnames, test_size=0.3)
+        tr_fnames, vl_fnames = train_test_split(tr_fnames, test_size=0.3)
+
+        self.tr_steps = len(tr_fnames) // self.BATCH_SIZE
+        self.vl_steps = len(vl_fnames) // self.BATCH_SIZE
+
+        tr_dset = tf.data.Dataset.from_generator(self.datagen, output_types=((tf.float32, tf.int64)),
+                                                 output_shapes=((self.IMG_SIZE, self.IMG_SIZE, 115),
+                                                                (self.IMG_SIZE, self.IMG_SIZE, 115)),
+                                                 args=(tr_fnames,))
+        self.tr_dset = tr_dset.batch(self.BATCH_SIZE, num_parallel_calls=self.DATA_PCALLS)\
+                         .prefetch(self.PREFETCH)
+
+        vl_dset = tf.data.Dataset.from_generator(self.datagen, output_types=((tf.float32, tf.int64)),
+                                                 output_shapes=((self.IMG_SIZE, self.IMG_SIZE, 115),
+                                                                (self.IMG_SIZE, self.IMG_SIZE, 115)),
+                                                 args=(vl_fnames,))
+        self.vl_dset = vl_dset.batch(self.BATCH_SIZE, num_parallel_calls=self.DATA_PCALLS)\
+                         .prefetch(self.PREFETCH)
+
+        ts_dset = tf.data.Dataset.from_generator(self.datagen, output_types=((tf.float32, tf.int64)),
+                                                 output_shapes=((self.IMG_SIZE, self.IMG_SIZE, 115),
+                                                                (self.IMG_SIZE, self.IMG_SIZE, 115)),
+                                                 args=(ts_fnames,))
+        self.ts_dset = ts_dset.batch(self.BATCH_SIZE, num_parallel_calls=self.DATA_PCALLS)\
+                         .prefetch(self.PREFETCH)
 
 
-lr_scheduler = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1,
-                                                    patience=3, verbose=1,
-                                                    mode='min', min_delta=1e-6, min_lr=0)
+    def datagen(self, fnames):
+        for i in range(len(fnames)):
+            img_fname = fnames[i].decode() + os.path.sep + fnames[i].decode() + "_t1.nii"
+            seg_fname = fnames[i].decode() + os.path.sep + fnames[i].decode() + "_seg.nii"
+            img = nibabel.load(join(self.DATA_DIR, img_fname))
+            img = img.get_fdata().astype(np.float32)
+            img = resize(img, [self.IMG_SIZE, self.IMG_SIZE, 115])
 
-t_nan = tf.keras.callbacks.TerminateOnNaN()
+            seg = nibabel.load(join(self.DATA_DIR, seg_fname))
+            seg = seg.get_fdata().astype(np.int64)
+            seg = resize(seg, [self.IMG_SIZE, self.IMG_SIZE, 115])
 
-es_cb = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=1e-3,
-                                         patience=9, verbose=1, mode='auto',
-                                         baseline=None, restore_best_weights=False,
-                                         start_from_epoch=0)
-
-checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(
-                            'checkpoints', monitor='val_loss', verbose=1,
-                            save_best_only=True, save_weights_only=False,
-                            mode='auto', save_freq='epoch')
-
-model.compile(optimizer="sgd", loss=tf.keras.losses.CategoricalCrossentropy(),
-              metrics=['accuracy'])
-
-model.fit(tr_dset, validation_data=vl_dset, epochs=EPOCHS, verbose=1,
-          steps_per_epoch=len(tr_fnames)//BATCH_SIZE,
-          validation_steps=len(vl_fnames)//BATCH_SIZE,
-          workers=10, use_multiprocessing=True,
-          callbacks=[lr_scheduler, t_nan, es_cb, checkpoint_cb])
+            yield img, seg
 
 
+    def build_model(self):
 
+        model = Sequential([InputLayer(input_shape=[self.IMG_SIZE, self.IMG_SIZE, 115, 1],
+                                       batch_size=None),
+                            Downsampling_block(),
+                            Upsampling_block(),
+                            Conv3D(1, (7,7,1), padding='valid'),
+                            Conv3D(1, (7,7,1), padding='valid'),
+                            Conv3D(1, (5,5,1), padding='valid'),
+                            Reshape([self.IMG_SIZE, self.IMG_SIZE, 115])
+                          ])
+
+        model.summary()
+        model.compile(optimizer="sgd", loss=tf.keras.losses.CategoricalCrossentropy(),
+                      metrics=['accuracy'])
+        return model
+
+    def train(self, model):
+        lr_scheduler = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1,
+                                                            patience=3, verbose=1,
+                                                            mode='min', min_delta=1e-6, min_lr=0)
+
+        t_nan = tf.keras.callbacks.TerminateOnNaN()
+
+        es_cb = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=1e-3,
+                                                 patience=9, verbose=1, mode='auto',
+                                                 baseline=None, restore_best_weights=False,
+                                                 start_from_epoch=0)
+
+        checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(
+                                    'checkpoints', monitor='val_loss', verbose=1,
+                                    save_best_only=True, save_weights_only=False,
+                                    mode='auto', save_freq='epoch')
+
+        model.fit(self.tr_dset, validation_data=self.vl_dset, epochs=self.EPOCHS, verbose=1,
+                  steps_per_epoch=self.tr_steps,
+                  validation_steps=self.vl_steps,
+                  workers=10, use_multiprocessing=True,
+                  callbacks=[lr_scheduler, t_nan, es_cb, checkpoint_cb])
+
+    def test_dataset(self):
+        for X, Y in self.tr_dset:
+            start_time = time.time()
+            print(X.shape)
+            print(Y.shape)
+            end_time = time.time()
+            elapsed = end_time - start_time
+            print(f"Time elapsed: {elapsed}")
+
+if __name__ == "__main__":
+    img_seg = ImageSegmentation()
+    model = img_seg.build_model()
+    # img_seg.train(model)
+    img_seg.test_dataset()
 
 
 
